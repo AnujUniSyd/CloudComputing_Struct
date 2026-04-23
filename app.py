07ebae78591ab996abd38dbcdaa9e642def5deb7
AnujUniSyd <amis0361@uni.sydney.edu.au>
2026-04-23 14:24:56 +1000

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import google.generativeai as genai
 # =========================================================
 
 AWS_REGION = "us-east-1"
-S3_BUCKET_NAME = "pdf-upload-assignement-1"
+S3_BUCKET_NAME = "pdf-upload-assignement-2"
 
 DB_HOST = "localhost"
 DB_PORT = 5432
